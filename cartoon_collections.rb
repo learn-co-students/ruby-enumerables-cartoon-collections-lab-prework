8a357c61d8a33f80b2e31a569b371d08848f2112
Mubarak Kareem <nelsonsmf@gmail.com>
2019-10-14 03:28:52 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each.with_index(1){|item, index|puts "#{index} #{item}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map{|item|"#{item.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  if array.any?{|word|word.length > 4}
+    return true 
+  end
+    return false
+  
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.detect{|word| word == "cheddar"|| word == "gouda"||word == "camembert"}
+return array.detect{|word| word == "cheddar"|| word == "gouda"||word == "camembert"}
+end
+
+return nil
+  
 end
