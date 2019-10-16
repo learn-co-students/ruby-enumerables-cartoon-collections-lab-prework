a6bcb7456494edc512a500f0dc2874d480b7edf6
Eli Kim <eliskim27@gmail.com>
2019-10-16 21:53:22 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,44 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (names)
+  names.each.with_index(1) do |dwarf,index|
+    puts "/#{index}. *#{dwarf}/"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+def summon_captain_planet (calls)
+  calls.map do |calls|
+    calls.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+def long_planeteer_calls (calls)
+  calls.any? do |obj|
+    obj.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+
+def find_the_cheese (foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  foods.find do |cheese|                
+    cheese_types.include? (cheese)      
+  end
 end
+
+# find method iterates over each value in foods and returns found value, or nil
+# what is it looking for?
+# include method iterates over each method in cheese_types 
+
+
+
+
+
+
+
+
+
