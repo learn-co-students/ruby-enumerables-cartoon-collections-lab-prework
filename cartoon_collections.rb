31d9385a89541159ddfebefedfc470127e050195
Beza Sirak <sirak.bezawit@gmail.com>
2020-01-24 15:19:46 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,10 @@
-def roll_call_dwarves (dwarves)
-  dwarves.each do |dwarve|
+def roll_call_dwarves (dwarfs)
+  dwarfs.each do |dwarf|
     i= 0 
     while i < drwarves.length
-    puts i + ".#{dwarve}"
+    puts i + ".#{dwarf}"
+    i +=1 
+    end
   end
 end
 
