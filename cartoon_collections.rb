812dad693c40b3de8d54a11f847f3e9b6141e765
Beza Sirak <sirak.bezawit@gmail.com>
2020-01-24 17:33:16 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,22 @@
-def roll_call_dwarves (dwarfs)
-  dwarfs.each do |dwarf|
-    i= 0 
-    while i < drwarves.length
-    puts i + ".#{dwarf}"
-    i +=1 
-    end
-  end
+def roll_call_dwarves (dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index +1}.#{dwarf}" 
+  end 
 end
 
+def summon_captain_planet(planeteer_calls)
+  new_array = planeteer_calls.map do |planeteer_call|
+  "#{planeteer_call.capitalize}!"
+  end
+  new_array
+end
 
+def long_planeteer_calls(array)
+  array.any? { |word| word.length > 4}
+end
 
+  
+def find_the_cheese(array)
+  array.find{|cheese| cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"}
+end 
 
