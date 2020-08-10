b0efb14742f6f03c644e368a63489544f54017aa
eliskim27 <eliskim27@gmail.com>
2020-08-10 12:37:55 -0400

    modify answer

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,12 @@
+# each with index and regex
 def roll_call_dwarves (names)
   names.each.with_index(1) do |dwarf,index|
-    puts "/#{index}. *#{dwarf}/"
+    puts "/#{index}. #{dwarf}/"
   end
 end
 
 
-
+# simple map
 def summon_captain_planet (calls)
   calls.map do |calls|
     calls.capitalize + "!"
@@ -13,7 +14,7 @@ def summon_captain_planet (calls)
 end
 
 
-
+# any? method 
 def long_planeteer_calls (calls)
   calls.any? do |obj|
     obj.length > 4
@@ -33,7 +34,8 @@ end
 # what is it looking for?
 # include method iterates over each method in cheese_types 
 
-
+# find returns first from block that is true
+# block: does current cheese match one of the cheese_types?
 
 
 
